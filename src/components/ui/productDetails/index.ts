68a0d376dbee0890f2f0d657f4ebf3100f7df2d9
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-18 11:43:26 +0600

    Structure Updated

---
+++ src/components/ui/productDetails/index.ts
@@ -0,0 +1,5 @@
+export { default as ProductImage } from "./ProductImage";
+export { default as ProductInfo } from "./ProductInfo";
+export { default as ProductActions } from "./ProductActions";
+export { default as ReviewsList } from "./ReviewsList";
+export { default as ReviewForm } from "./ReviewForm";
