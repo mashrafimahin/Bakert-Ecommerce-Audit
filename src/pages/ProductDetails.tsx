c85978a967de40f1dbee3d612bbd354dd749d7e7
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-18 11:36:30 +0600

    Structure Updated

---
+++ src/pages/ProductDetails.tsx
@@ -0,0 +1,26 @@
+// dependencies
+import { type FC, useEffect } from "react";
+// components
+import Navbar from "../components/ui/navbar";
+import Footer from "../components/ui/footer";
+// layouts
+import ProductDetailsLayout from "../layouts/productDetails";
+
+// main
+const ProductDetails: FC = () => {
+  // auto scroll
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <ProductDetailsLayout />
+      <Footer />
+    </>
+  );
+};
+
+// exports
+export default ProductDetails;
