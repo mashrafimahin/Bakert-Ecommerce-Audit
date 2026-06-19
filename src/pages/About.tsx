753b33fef1091481b01639dcf41c8fa70701fe79
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-19 10:01:16 +0600

    Structure Updated

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,5 @@
 // dependencies
-import type { FC } from "react";
+import { type FC, useEffect } from "react";
 // components
 import Navbar from "../components/ui/navbar";
 import Footer from "../components/ui/footer";
@@ -8,6 +8,11 @@ import AboutLayout from "../layouts/AboutLayout";
 
 // main
 const About: FC = () => {
+  // auto scroll
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <Navbar />
