bcabf9f0889d876d05ecef2dd32447a6dfd7c884
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-19 19:59:07 +0600

    Structure Updated : New Slice Added

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 // slices
 import globalReducer from "../features/globalController";
 import productReducer from "../features/productController";
+import DashboardReducer from "../features/dashboardController";
 
 export const store = configureStore({
   reducer: {
     globalController: globalReducer,
     productController: productReducer,
+    dashboardController: DashboardReducer,
   },
 });
 
