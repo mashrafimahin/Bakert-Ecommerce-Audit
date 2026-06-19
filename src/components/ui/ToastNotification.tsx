3fd4713da28125fbf7c2b732d479917154a9d7c8
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-19 22:31:43 +0600

    Structure Updated

---
+++ src/components/ui/ToastNotification.tsx
@@ -0,0 +1,56 @@
+// dependencies
+import type { FC } from "react";
+// utilities
+import { cn } from "../../utils/ClassMerger";
+// icons
+import { ShieldAlert, ShieldCheck, ShieldX } from "lucide-react";
+// components
+import Typography from "../typography";
+import useSlices from "../../hooks/useSlices";
+// pattern - icon
+const IconPattern = {
+  success: {
+    icon: ShieldCheck,
+    color: "text-green-600",
+  },
+  warning: {
+    icon: ShieldAlert,
+    color: "text-amber-500",
+  },
+  error: {
+    icon: ShieldX,
+    color: "text-red-600",
+  },
+};
+
+// main
+const ToastNotification: FC = () => {
+  // state
+  const { data } = useSlices("globalController");
+  const show = data.showNotification;
+  const mark = data.notificationType as keyof typeof IconPattern;
+
+  // select icon
+  const Icon = IconPattern[mark].icon;
+
+  return (
+    <>
+      <div
+        className={cn(
+          "absolute left-1/2 -translate-x-1/2 -translate-y-1/2 mt-4 bg-white shadow-2xl border-2 rounded-2xl py-3 px-6 z-70 transition-all",
+          show ? "top-8" : "-top-20",
+        )}
+      >
+        <div className="flex items-center gap-2">
+          <Icon size={20} className={cn(IconPattern[mark].color)} />
+          <Typography className="text-md text-gray-800 font-semibold">
+            {data.notificationMessage}
+          </Typography>
+        </div>
+      </div>
+    </>
+  );
+};
+
+// exports
+export default ToastNotification;
