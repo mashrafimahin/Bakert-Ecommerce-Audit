b3342768c54d160b2d93f801f9445e275b5ac15c
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-23 10:05:15 +0600

    Data Structure Updated

--- src/handlers/dashboardHandler.ts
+++ src/handlers/dashboardHandler.ts
@@ -1,37 +1,39 @@
-// Dashboard Data Handler
-
 // main
 export const dashboardHandler = async () => {
-  try {
-    // ordered list
-    const ORDERED_LIST = [
-      {
-        id: "1",
-        orderedDate: "June 12, 2026",
-        status: "Delivered",
-        category: "Cakes",
-        image:
-          "https://images.unsplash.com/photo-1606983340126-99ab4feaa64a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjaG9jb2xhdGUlMjBjYWtlfGVufDF8fHx8MTc4MTU4NTc4Mnww&ixlib=rb-4.1.0&q=80&w=1080",
-        name: "Decadent Chocolate Truffle",
-        quantity: 2,
-        price: 45,
-        rating: 4.8,
-        reviews: 124,
-      },
-      {
-        id: "2",
-        orderedDate: "June 12, 2026",
-        status: "Delivered",
-        category: "Cakes",
-        image:
-          "https://images.unsplash.com/photo-1606983340126-99ab4feaa64a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjaG9jb2xhdGUlMjBjYWtlfGVufDF8fHx8MTc4MTU4NTc4Mnww&ixlib=rb-4.1.0&q=80&w=1080",
-        name: "Decadent Chocolate Truffle",
-        quantity: 2,
-        price: 45,
-      },
-    ];
+  const userId = localStorage.getItem("user_access");
 
-    return { orders: [...ORDERED_LIST], favorites: [...ORDERED_LIST] };
+  if (!userId) {
+    return {
+      success: false,
+    };
+  }
+
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_KEY}/userProducts`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId }),
+        credentials: "include",
+      },
+    );
+    const data = await response.json();
+    // modify data
+    const modifiedFavs = [];
+
+    // set product_id -> id
+    for (const item of data.favorites) {
+      item.id = item.product_id;
+      modifiedFavs.push(item);
+    }
+
+    return {
+      orders: [...data.orders],
+      favorites: modifiedFavs,
+    };
   } catch (err) {
     console.log(err);
     return { orders: [], favorites: [] };
