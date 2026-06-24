4e27363b2386b3fe05a20aa71c18f91b0c823ef6
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-24 12:10:24 +0600

    Structure Updated : Post Review Function Added

--- src/handlers/toggleHandler.ts
+++ src/handlers/toggleHandler.ts
@@ -1,3 +1,6 @@
+// dependencies
+import type { Review } from "../app/features/productController";
+
 // add products as favorite
 export const addFavorite = async (productId: string) => {
   try {
@@ -41,5 +44,29 @@ export const removeFavorite = async (productId: string) => {
   } catch (err) {
     console.log(err);
     return { success: false };
+  }
+};
+
+// post review
+export const postNewReview = async (reviewData: Review) => {
+  try {
+    const userId = localStorage.getItem("user_access");
+    const response = await fetch(
+      `${import.meta.env.VITE_API_KEY}/product/placeReview`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId, ...reviewData }),
+        credentials: "include",
+      },
+    );
+    const data = await response.json();
+    console.log(data);
+    // response
+    // return refinedData;
+  } catch (err) {
+    console.log(err);
   }
 };
